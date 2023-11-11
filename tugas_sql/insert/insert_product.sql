08a28fafc331bea35c3a77917367fb555b5fa3be
Muhammad Jalil Asyakur <67129177+MuhammadJalilAsyakur@users.noreply.github.com>
2023-11-11 23:36:01 +0700

    insert data product

---
+++ tugas_sql/insert/insert_product.sql
@@ -0,0 +1,13 @@
+INSERT INTO `product` (`product_name`, `price`, `stok`, `category_id`) VALUES
+('Laptop Acus LOG', 1200.00, 50, 1),
+('Erigo X JKT 48 Freya', 19.99, 100, 2),
+('The Book Of Jungler', 9.99, 75, 3),
+('Garden Hose', 29.99, 30, 4),
+('Ball', 12.50, 50, 5),
+('Action Figure - Deadpool', 8.99, 40, 6),
+('Shampoo BANGO', 5.49, 60, 7),
+('Car Oil', 14.99, 20, 8),
+('Vitamin C Supplements', 17.95, 100, 9),
+('Chocolate Bar', 2.99, 200, 10);
+
+select * from product;
