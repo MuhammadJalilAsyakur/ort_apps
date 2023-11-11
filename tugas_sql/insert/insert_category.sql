63b7491802937c34ace40a262f74a754bfdca5fc
Muhammad Jalil Asyakur <67129177+MuhammadJalilAsyakur@users.noreply.github.com>
2023-11-11 23:32:30 +0700

    Insert data category

---
+++ tugas_sql/insert/insert_category.sql
@@ -0,0 +1,14 @@
+INSERT INTO `category` (`category_name`) VALUES
+('Electronics'),
+('Clothing'),
+('Books'),
+('Home and Garden'),
+('Sports and Outdoors'),
+('Toys and Games'),
+('Beauty and Personal Care'),
+('Automotive'),
+('Health and Wellness'),
+('Food and Beverages');
+
+
+select * from category;
