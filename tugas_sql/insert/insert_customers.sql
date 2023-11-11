da3e72597671f1f40fde8e489c3758a2fc2a5d05
Muhammad Jalil Asyakur <67129177+MuhammadJalilAsyakur@users.noreply.github.com>
2023-11-11 21:58:53 +0700

    insert data customers
    insert data

---
+++ tugas_sql/insert/insert_customers.sql
@@ -0,0 +1,14 @@
+insert into customer values 
+(1187, 'Adam Tua', 'akuTua123' , 'adam@gmail.com', '0821928421', 'Jl. Old Man'),
+(1188, 'Gavi', 'akuGavi123' ,'Gavi@gmail.com', '0878734081', 'Jl. Catalunia'),
+(1189, 'Marc Marquez', 'akuJatuh123' , 'Markues@gmail.com', '0817772111', 'Jl. Ndolosor'),
+(1190, 'Pecco', 'akuJurdun123' , 'pecco@gmail.com', '0883455561', 'Jl. Ducati'),
+(1200, 'Martin', 'akuMartin123' , 'martin@gmail.com', '0832315439', 'Jl. Pramac'),
+(1201, 'Rusdi', 'akuRusdi123' , 'rusdi@gmail.com', '0882422443', 'Jl. Barber'),
+(1222, 'Billington', 'akuSiapa123' , 'bill@gmail.com', '0822842148', 'Jl. Crewe'),
+(1231, 'Michiwaki', 'akuJepang123' , 'waki@gmail.com', '0898211341', 'Jl. Jepun'),
+(1247, 'Adam Uchiha', 'akuUchiha123' , 'uchiha@gmail.com', '0837725261', 'Jl. Konoha'),
+(1317, 'Kiryu', 'akuYakuza123' , 'kiryu@gmail.com', '0821934142', 'Jl. Dojima');
+
+
+select * from customer;
