7e8e05d62861ccb5660230a7197a6f052313054e
Muhammad Jalil Asyakur <67129177+MuhammadJalilAsyakur@users.noreply.github.com>
2023-11-12 15:47:12 +0700

    insert data to order_detail

---
+++ tugas_sql/insert/insert_order_detail.sql
@@ -0,0 +1,13 @@
+INSERT INTO order_detail (customer_id, product_id, payment_id) VALUES
+(1187, 1, 1),
+(1188, 2, 2),
+(1189, 3, 3),
+(1190, 4, 4),
+(1200, 5, 5),
+(1201, 6, 6),
+(1222, 7, 7),
+(1231, 8, 8),
+(1247, 9, 9),
+(1317, 10, 10);
+
+select * from order_detail;
